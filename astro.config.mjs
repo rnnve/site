a02c1336f96092309eab684a30a6886a3979e466
rnnve <fuyumi.am@gmail.com>
2026-09-09 21:15:41 +0700

    feat: update font imports and remove unused wrangler config

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
   integrations: [react(), sitemap({ customPages: ['https://maplenan.org/'] })],
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
   adapter: vercel()
