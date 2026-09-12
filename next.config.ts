df1e8fdc5c7f9233718e02e2aab3607eca0f9b8d
rnnve <fuyumi.am@gmail.com>
2026-09-12 15:51:43 +0700

    config(next): proxy hub paths and allow LAN dev origin

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  
+  async rewrites() {
+  return [
+    { source: '/hub/p.js', destination: 'https://portus.sh/p.js' },
+    { source: '/hub/e', destination: 'https://portus.sh/api/e' },
+  ]
+},
+
+  allowedDevOrigins: ['192.168.1.39'],
 };
 
 export default nextConfig;
