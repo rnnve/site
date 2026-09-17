0ff7141cf2747486a828a197b238d7883294064b
rnnve <fuyumi.am@gmail.com>
2026-09-17 07:37:17 +0700

    refactor(layout): unmount CursorTooltip

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 			<body className="flex h-dvh min-w-0 flex-col overflow-hidden bg-surface font-sans">
 				<SiteNav />
 				<div className="flex min-h-0 flex-1 flex-col overflow-hidden">{children}</div>
-				<CursorTooltip />
 				<Analytics />
 				<SpeedInsights />
 				<Script
