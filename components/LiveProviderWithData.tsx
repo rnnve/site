63456b18237734ca6a40711c652afd00635f265c
rnnve <fuyumi.am@gmail.com>
2026-09-17 07:39:09 +0700

    feat(LiveProviderWithData): mount LiveProvider with data

--- components/LiveProviderWithData.tsx
+++ components/LiveProviderWithData.tsx
@@ -10,3 +10,4 @@ interface LiveProviderWithDataProps {
 export async function LiveProviderWithData({ children, views }: LiveProviderWithDataProps) {
 	const initialData = await getInitialLiveData(views);
 	return (
+		<LiveProvider initialData={initialData} views={views}>
