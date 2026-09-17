5ae8577470dabf475a5ae4ac2803f8b64cffd7b9
rnnve <fuyumi.am@gmail.com>
2026-09-17 08:09:59 +0700

    feat(cover-art): type: track | artist,

--- lib/cover-art.ts
+++ lib/cover-art.ts
@@ -42,3 +42,4 @@ async function fetchJson<T>(url: string, userAgent = 'site/1.0'): Promise<T | nu
 export async function resolveFastCover(
 	name: string,
 	artist: string,
+	type: 'track' | 'artist',
