72bebed0b641aa0fdccb31e21670277f83cd6137
rnnve <fuyumi.am@gmail.com>
2026-09-17 07:41:03 +0700

    refactor(CursorTooltip): remove document.removeEventListener(mouseleave, onLeave

--- components/CursorTooltip.tsx
+++ components/CursorTooltip.tsx
@@ -46,4 +46,3 @@ export default function CursorTooltip() {
 		document.addEventListener('mouseleave', onLeave);
 		return () => {
 			document.removeEventListener('pointermove', onMove);
-			document.removeEventListener('mouseleave', onLeave);
