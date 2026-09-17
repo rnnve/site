c14846727a1740a31ffb437fe2215915a9229185
rnnve <fuyumi.am@gmail.com>
2026-09-17 07:40:09 +0700

    style(music/page): remove blank line after LiveProvider import

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -2,7 +2,6 @@ import { Suspense } from 'react';
 import LastFmWidget from '@/components/LastFmWidget';
 import SiteFooter from '@/components/SiteFooter';
 import { LiveProvider } from '@/components/LiveProvider';
-
 export const dynamic = 'force-dynamic';
 
 export const metadata = {
