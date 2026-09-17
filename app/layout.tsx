0a3077c258d8930c12dcc989a800e48dadc0d656
rnnve <fuyumi.am@gmail.com>
2026-09-17 07:37:07 +0700

    refactor(layout): remove CursorTooltip import

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import { GeistMono } from 'geist/font/mono';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import SiteNav from '@/components/SiteNav';
-import CursorTooltip from '@/components/CursorTooltip';
 import '@/styles/global.css';
 
 export const metadata: Metadata = {
