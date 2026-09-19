649f37756211a5f72823ebf09c5ce96fab9abfcc
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:09 +0700

    refactor(SiteNav): remove line: href: string

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -35,7 +35,6 @@ function MenuIcon({ className }: { className?: string }) {
 	href: string;
 	Icon: ComponentType<{ className?: string }>;
 }
-	href: string
 const containerStyle: CSSProperties = {
 	background: 'rgba(0,0,0,0.8)',
 	backdropFilter: 'blur(24px)',
