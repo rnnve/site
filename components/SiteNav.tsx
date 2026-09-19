9f7b81283edc49e24a443b2035c13e59e0b0904a
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:10 +0700

    refactor(SiteNav): remove line: )}

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -74,7 +74,6 @@ function NavButton({
 				<motion.span
 					transition={pillTransition}
 				/>
-			)}
 				whileHover={{ scale: 1.08 }}
 				whileTap={{ scale: 0.95 }}
 				transition={{ duration: 0.15, ease: 'easeOut' }}
