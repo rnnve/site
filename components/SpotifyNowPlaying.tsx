32ddae7d13080dac5640c8cd1fe0448db267427f
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:14 +0700

    feat(SpotifyNowPlaying): add line: <span className="font-semibold text-on-surface-variant">{t.spotifyLabe

--- components/SpotifyNowPlaying.tsx
+++ components/SpotifyNowPlaying.tsx
@@ -38,6 +38,7 @@ export default function SpotifyNowPlayingCard() {
 	}
 
 	if (spotifyError) {
+				<span className="font-semibold text-on-surface-variant">{t.spotifyLabel}</span>
 		return (
 				<p className="mt-1">Unable to load: {spotifyError}</p>
 			</div>
