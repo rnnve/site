0dd9f61323e64e1bcdfdfa35de0e7a27e885069b
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:07 +0700

    refactor(LastFmWidget): remove line: key={id}

--- components/LastFmWidget.tsx
+++ components/LastFmWidget.tsx
@@ -186,7 +186,6 @@ function ViewTabs({ view, setView, t }: { view: LastFmView; setView: (v: LastFmV
 			<div className="flex w-full min-w-0 flex-wrap gap-1.5 px-1">
 				{VIEWS.map(({ id, label }) => (
 					<button
-						key={id}
 	const registeredYear = user.registered?.unixtime
 		? new Date(Number(user.registered.unixtime) * 1000).getUTCFullYear()
 		: null;
