501b3c51fe70591fd23312eca4581840b81b5400
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:12 +0700

    feat(SiteNav): add line: };

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -176,6 +176,7 @@ export default function SiteNav() {
 				currentY = scroller ? scroller.scrollTop : window.scrollY;
 			}
 			setScrolled(currentY > 15);
+		};
 	const isActive = (href: string) => (href === '/' ? pathname === '/' : pathname.startsWith(href));
 
 	return (
