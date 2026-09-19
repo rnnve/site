57032fbc6c8e22d14f3f45a2598e8a856a80f2cc
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:13 +0700

    feat(SiteNav): add line: isMobile={true}

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -271,6 +271,7 @@ export default function SiteNav() {
 										active={isActive(item.href)}
 										pillId="nav-pill"
 										collapsed={false}
+										isMobile={true}
 
 	return (
 		<MotionConfig reducedMotion="user">
