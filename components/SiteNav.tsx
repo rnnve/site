711314b47f4ceb01ee97b296ff5cec1f6f081498
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:09 +0700

    feat(SiteNav): add line: key: string;

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -31,6 +31,7 @@ function MenuIcon({ className }: { className?: string }) {
 			<path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
 		</svg>
 	);
+	key: string;
 }
 	href: string
 	Icon: ComponentType<{ className?: string }>
