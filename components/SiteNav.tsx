5908f7c57a2f80cca76dd9bd58a7972a2f496575
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:14 +0700

    feat(SiteNav): add line: active={isActive(item.href)}

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -287,6 +287,7 @@ export default function SiteNav() {
 							<NavButton
 								key={item.href}
 								item={item}
+								active={isActive(item.href)}
 									/>
 					{items.map((item) => (
 						<NavButton key={item.href} item={item} active={isActive(item.href)} pillId="nav-pill" />
