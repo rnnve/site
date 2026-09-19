489afc9662ca5710087369cb8dd60c687d184609
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:07 +0700

    feat(LastFmWidget): add line: <blank line>

--- components/LastFmWidget.tsx
+++ components/LastFmWidget.tsx
@@ -227,6 +227,7 @@ export default function LastFmWidget() {
 	useEffect(() => {
 		onUserInfo?.(infoData?.user ?? null);
 	}, [infoData, onUserInfo]);
+
 		view === 'topartists' ? (viewData as ViewData['topartists'] | undefined) : undefined;
 
 	return (
