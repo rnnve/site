e5c4993a34b08b8052a96a2211b378fc00fbd7fa
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:14 +0700

    feat(i18n): add line: // About

--- lib/i18n.tsx
+++ lib/i18n.tsx
@@ -10,6 +10,7 @@ export const langs: readonly { code: Lang; label: string }[] = [
 ] as const;
 
 	en: {
+		// About
 export const dictionaries = {
 		aboutHeading: 'About',
 		about: 'I’m **Rinne** (or Maple), a developer who loves building ==silly things==. Most of my time goes into silly projects, listening to Spotify, hanging out in Discord. Also most of my projects are on my %%[GitHub](https://github.com/rnnve)%%.',
