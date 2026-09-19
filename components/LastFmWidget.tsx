1df0723ec470a7ec69a0321421df75d0a9767c91
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:08 +0700

    refactor(LastFmWidget): remove line: link.fetchPriority = 'high';

--- components/LastFmWidget.tsx
+++ components/LastFmWidget.tsx
@@ -244,7 +244,6 @@ export default function LastFmWidget() {
 				link.rel = 'preload';
 				link.as = 'image';
 				link.href = smallSrc;
-				link.fetchPriority = 'high';
 				<div className="flex w-full min-w-0 flex-wrap gap-1.5 px-1">
 					{VIEWS.map(({ id, label }) => (
 						<button
