86be47b5169674db990323d84f681f78faea2c32
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:01 +0700

    feat(layout): add line: <DocumentLanguageSync />

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,6 +58,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 					<link key={href} rel="preconnect" href={href} crossOrigin="anonymous" />
 				))}
 				<I18nProvider>
+					<DocumentLanguageSync />
 			</head>
 				<div className="flex min-h-0 flex-1 flex-col overflow-hidden">{children}</div>
 				<Analytics />
