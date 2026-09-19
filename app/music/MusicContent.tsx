f2b882138aca6fa8ef9db7d4ab7c9a2d1bb65883
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:16 +0700

    feat(music/MusicContent): add line: <blank line>

--- app/music/MusicContent.tsx
+++ app/music/MusicContent.tsx
@@ -10,3 +10,4 @@ import type { LastFmUserInfo } from '@/lib/integrations';
 function Stat({ label, value }: { label: string; value: string }) {
 	const [displayValue, setDisplayValue] = useState(0);
 	const targetValue = Number(value.replace(/,/g, ''));
+
