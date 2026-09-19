afb1bdb53e92e389b53797cbee173377d4280932
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:19 +0700

    feat(LanguageSwitcher): add line: key={code}

--- components/LanguageSwitcher.tsx
+++ components/LanguageSwitcher.tsx
@@ -23,3 +23,4 @@ export default function LanguageSwitcher() {
 			<div style={containerStyle} className="flex items-center gap-0.5 rounded-xl p-0.5">
 				{langs.map(({ code, label }) => (
 					<button
+						key={code}
