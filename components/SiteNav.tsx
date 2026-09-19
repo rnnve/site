90694874bcb8346aae2466ff485ef45a916f4539
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:12 +0700

    feat(SiteNav): add line: <blank line>

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -195,6 +195,7 @@ export default function SiteNav() {
 	const collapsed = !isMobile && scrolled;
 	const isActive = (href: string) => (href === '/' ? pathname === '/' : pathname.startsWith(href));
 
+
 	return (
 		<MotionConfig reducedMotion="user">
 			<nav
