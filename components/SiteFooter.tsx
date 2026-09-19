2299373b79bf966f3663b15926c77df03484a806
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:08 +0700

    refactor(SiteFooter): remove line: @rnnve/site # {commits} commits

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -31,7 +31,6 @@ export default async function SiteFooter({ className }: { className?: string }) 
 						rel="noreferrer"
 						className="text-blush py-1"
 					>
-						@rnnve/site # {commits} commits
 					target="_blank"
 					rel="noreferrer"
 					className="footer-rainbow-link inline-flex min-h-11 items-center rounded-md px-2"
