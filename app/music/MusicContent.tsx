d390504133ecd4263d6c869baedf0e4b515b384a
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:16 +0700

    feat(music/MusicContent): add line: <blank line>

--- app/music/MusicContent.tsx
+++ app/music/MusicContent.tsx
@@ -6,3 +6,4 @@ import LastFmWidget from '@/components/LastFmWidget';
 import SiteFooter from '@/components/SiteFooter';
 import { useI18n } from '@/lib/i18n';
 import type { LastFmUserInfo } from '@/lib/integrations';
+
