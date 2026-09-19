143483d2e9def94fb1fe2d714da18276be6204fc
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:08 +0700

    refactor(SiteNav): remove line: import type { CSSProperties, ComponentType } from 'react';

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -3,7 +3,6 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { motion, MotionConfig } from 'framer-motion';
-import type { CSSProperties, ComponentType } from 'react';
 
 function HomeIcon({ className }: { className?: string }) {
 	return (
