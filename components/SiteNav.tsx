4df4662dbab72be936aee7f16069b47bff5b6743
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:12 +0700

    feat(SiteNav): add line: padding: '0.5rem 1rem',

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -234,6 +234,7 @@ export default function SiteNav() {
 							className="flex items-center justify-between"
 							style={{
 								...containerStyle,
+								padding: '0.5rem 1rem',
 
 	return (
 		<MotionConfig reducedMotion="user">
