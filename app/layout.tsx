877edfd91fe5bc7582865a9f15cbe3d4c039dcd3
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:01 +0700

    feat(layout): add line: import { Chakra_Petch } from 'next/font/google';

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from 'next';
 import Script from 'next/script';
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
+import { Chakra_Petch } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import SiteNav from '@/components/SiteNav';
