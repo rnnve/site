875d2986e196c1df94ceaedf953a7788db57c4d1
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:14 +0700

    feat(SiteNav): add line: <LanguageSwitcherBar />

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -294,6 +294,7 @@ export default function SiteNav() {
 							/>
 						))}
 					</div>
+					<LanguageSwitcherBar />
 									/>
 					{items.map((item) => (
 						<NavButton key={item.href} item={item} active={isActive(item.href)} pillId="nav-pill" />
