ccfafd7490992882436323cf87d27d4bdd3d2e22
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:03 +0700

    refactor(AboutSection): remove line: .replace(/\|\|([^|]+)\|\|/g, '<span class="spoiler">$1</span>');

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -10,7 +10,6 @@ import { dictionaries, langs, useI18n } from '@/lib/i18n';
 function discordify(markdown: string): string {
 	return markdown
 		.replace(/__([^_]+)__/g, '<u>$1</u>')
-		.replace(/\|\|([^|]+)\|\|/g, '<span class="spoiler">$1</span>');
 				<h2 className="text-xs font-medium uppercase tracking-wider text-on-surface-variant">
 					{t.aboutHeading}
 				</h2>
