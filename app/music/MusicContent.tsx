5c66de999d29c921cc6b3ba97386d825d3be732f
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:17 +0700

    feat(music/MusicContent): add line: <Stat label={t.lastfmStats.albums} value={user.album_count} />

--- app/music/MusicContent.tsx
+++ app/music/MusicContent.tsx
@@ -59,3 +59,4 @@ function StatsHeader({ user, t }: { user: LastFmUserInfo; t: ReturnType<typeof u
 				<Stat label={t.lastfmStats.scrobbles} value={user.playcount} />
 				<Stat label={t.lastfmStats.tracks} value={user.track_count} />
 				<Stat label={t.lastfmStats.artists} value={user.artist_count} />
+				<Stat label={t.lastfmStats.albums} value={user.album_count} />
