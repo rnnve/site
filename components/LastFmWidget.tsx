8f02a4f25e5b780deca6d9cf760cb5f126faaabf
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:07 +0700

    feat(LastFmWidget): add line: const { t } = useI18n();

--- components/LastFmWidget.tsx
+++ components/LastFmWidget.tsx
@@ -185,6 +185,7 @@ function ViewTabs({ view, setView, t }: { view: LastFmView; setView: (v: LastFmV
 		<div className="-mx-1 shrink-0 bg-surface pb-2 pt-0.5">
 			<div className="flex w-full min-w-0 flex-wrap gap-1.5 px-1">
 export default function LastFmWidget({ initialView = 'toptracks', onViewChange, onUserInfo }: LastFmWidgetProps) {
+	const { t } = useI18n();
 				{VIEWS.map(({ id, label }) => (
 	const registeredYear = user.registered?.unixtime
 		? new Date(Number(user.registered.unixtime) * 1000).getUTCFullYear()
