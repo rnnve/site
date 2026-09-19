9519fa9d5d6606bb873f7ddec955aa9734bbb4db
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:01 +0700

    feat(layout): add line: <SiteNav />

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 				))}
 				<I18nProvider>
 					<DocumentLanguageSync />
+					<SiteNav />
 			</head>
 				<div className="flex min-h-0 flex-1 flex-col overflow-hidden">{children}</div>
 				<Analytics />
