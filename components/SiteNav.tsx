6cb71aff8163e5e16685f099eb6918a515c84624
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:12 +0700

    feat(SiteNav): add line: className="flex items-center justify-between"

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -231,6 +231,7 @@ export default function SiteNav() {
 						<motion.div
 							layout
 							transition={springTransition}
+							className="flex items-center justify-between"
 
 	return (
 		<MotionConfig reducedMotion="user">
