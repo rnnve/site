5b4e230947b570c4b256db80f5d03f6784636492
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:13 +0700

    feat(SiteNav): add line: exit={{ opacity: 0, height: 0 }}

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -247,6 +247,7 @@ export default function SiteNav() {
 						<motion.div
 							initial={{ opacity: 0, height: 0 }}
 							animate={{ opacity: 1, height: 'auto' }}
+							exit={{ opacity: 0, height: 0 }}
 
 	return (
 		<MotionConfig reducedMotion="user">
