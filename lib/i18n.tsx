8d71f07bcaf2a940b90960a2b49c1a1dbf92eab3
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:14 +0700

    feat(i18n): add line: // Site metadata

--- lib/i18n.tsx
+++ lib/i18n.tsx
@@ -14,6 +14,7 @@ export const langs: readonly { code: Lang; label: string }[] = [
 export const dictionaries = {
 		about: 'I\'m **Rinne** (or Maple), a developer who loves building ==silly things==. Most of my time goes into silly projects, listening to Spotify, hanging out in Discord. Also most of my projects are on my %%[GitHub](https://github.com/rnnve)%%.',
 
+		// Site metadata
 		aboutHeading: 'About',
 	},
 	th: {
