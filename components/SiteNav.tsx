12ebf12d61e130e089bd041b520452a0fad68d6c
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:10 +0700

    feat(SiteNav): add line: className="overflow-hidden whitespace-nowrap text-sm font-medium"

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -94,6 +94,7 @@ function NavButton({
 						initial={false}
 						animate={{ opacity: 1, width: 'auto' }}
 						transition={{ duration: 0.2, ease: 'easeInOut' }}
+						className="overflow-hidden whitespace-nowrap text-sm font-medium"
 			>
 			</motion.span>
 		</Link>
