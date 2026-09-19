ec790dcc150ebb41cef7964c549f4e6c29eb426f
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:16 +0700

    feat(music/MusicContent): add line: import { useI18n } from '@/lib/i18n';

--- app/music/MusicContent.tsx
+++ app/music/MusicContent.tsx
@@ -4,3 +4,4 @@ import { Suspense } from 'react';
 import { useEffect, useState } from 'react';
 import LastFmWidget from '@/components/LastFmWidget';
 import SiteFooter from '@/components/SiteFooter';
+import { useI18n } from '@/lib/i18n';
