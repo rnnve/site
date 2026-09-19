4d38a406257aeb33d76542e773622694829f5292
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:13 +0700

    refactor(SiteNav): remove line: </motion.div>

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -275,7 +275,6 @@ export default function SiteNav() {
 									/>
 								))}
 							</motion.div>
-						</motion.div>
 					{items.map((item) => (
 						<NavButton key={item.href} item={item} active={isActive(item.href)} pillId="nav-pill" />
 					))}
