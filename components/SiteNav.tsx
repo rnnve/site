830730fa0564fb9350b18839d890482e5f03e3b2
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:12 +0700

    feat(SiteNav): add line: }}

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -236,6 +236,7 @@ export default function SiteNav() {
 								...containerStyle,
 								padding: '0.5rem 1rem',
 								borderRadius: 0,
+							}}
 
 	return (
 		<MotionConfig reducedMotion="user">
