ba2e30468a82edffcf85f9ac2d5fb85821314a9e
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:12 +0700

    feat(SiteNav): add line: <LanguageSwitcherBar />

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -240,6 +240,7 @@ export default function SiteNav() {
 						>
 							<BurgerButton onClick={() => setMobileMenuOpen(true)} expanded={false} />
 							<div className="flex-1" />
+							<LanguageSwitcherBar />
 
 	return (
 		<MotionConfig reducedMotion="user">
