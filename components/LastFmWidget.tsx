09eea8c146ad6c0a9d5c25d0327c75d64dcc4d95
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:07 +0700

    feat(LastFmWidget): add line: document.head.appendChild(link);

--- components/LastFmWidget.tsx
+++ components/LastFmWidget.tsx
@@ -245,6 +245,7 @@ export default function LastFmWidget() {
 				link.as = 'image';
 				link.href = smallSrc;
 				link.fetchPriority = 'high';
+				document.head.appendChild(link);
 		view === 'topartists' ? (viewData as ViewData['topartists'] | undefined) : undefined;
 
 	return (
