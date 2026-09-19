10f00785bb3eb38c1443083b98fe88d7c339a779
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:09 +0700

    refactor(SiteNav): remove line: }

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -34,7 +34,6 @@ function MenuIcon({ className }: { className?: string }) {
 	key: string;
 	href: string;
 	Icon: ComponentType<{ className?: string }>;
-}
 const containerStyle: CSSProperties = {
 	background: 'rgba(0,0,0,0.8)',
 	backdropFilter: 'blur(24px)',
