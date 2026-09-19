1ed49c017c0df24da61e2a57381c6a141feb5221
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:06 +0700

    feat(LastFmWidget): add line: ];

--- components/LastFmWidget.tsx
+++ components/LastFmWidget.tsx
@@ -181,6 +181,7 @@ function ViewTabs({ view, setView, t }: { view: LastFmView; setView: (v: LastFmV
 		{ id: 'toptracks', label: t.lastfmViews.toptracks },
 		{ id: 'topartists', label: t.lastfmViews.topartists },
 		{ id: 'recent', label: t.lastfmViews.recent },
+	];
 
 	return (
 		<div className="min-w-0 rounded-lg bg-surface-container-high/60 px-3 py-2.5">
