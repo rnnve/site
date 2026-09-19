0b88c5a285f6dca6aeac8f4df31efc06c148b4ea
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:12 +0700

    feat(SiteNav): add line: style={{

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -232,6 +232,7 @@ export default function SiteNav() {
 							layout
 							transition={springTransition}
 							className="flex items-center justify-between"
+							style={{
 
 	return (
 		<MotionConfig reducedMotion="user">
