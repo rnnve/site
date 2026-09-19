7a8c968022f6b8f06efaa18a68765025d98ea2e5
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:14 +0700

    refactor(i18n): remove line: en: {

--- lib/i18n.tsx
+++ lib/i18n.tsx
@@ -10,7 +10,6 @@ export const langs: readonly { code: Lang; label: string }[] = [
 ] as const;
 
 export const dictionaries = {
-  en: {
 		aboutHeading: 'About',
 		about: 'I’m **Rinne** (or Maple), a developer who loves building ==silly things==. Most of my time goes into silly projects, listening to Spotify, hanging out in Discord. Also most of my projects are on my %%[GitHub](https://github.com/rnnve)%%.',
 	},
