6fc4ad74e56ab6ec3a377dbdb3e6ca1c2ed74a96
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:13 +0700

    feat(SiteNav): add line: active={isActive(item.href)}

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -268,6 +268,7 @@ export default function SiteNav() {
 									<NavButton
 										key={item.href}
 										item={item}
+										active={isActive(item.href)}
 
 	return (
 		<MotionConfig reducedMotion="user">
