327547ec208fbca79b678958c4695f4bbc59aac6
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:06 +0700

    refactor(LastFmWidget): remove line: <blank line>

--- components/LastFmWidget.tsx
+++ components/LastFmWidget.tsx
@@ -199,7 +199,6 @@ function ViewTabs({ view, setView, t }: { view: LastFmView; setView: (v: LastFmV
 					</button>
 				))}
 			</div>
-
 	const registeredYear = user.registered?.unixtime
 		? new Date(Number(user.registered.unixtime) * 1000).getUTCFullYear()
 		: null;
