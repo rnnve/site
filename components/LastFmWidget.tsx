636714d1bf42c2eada20db8475815900c6b95054
rnnve <fuyumi.am@gmail.com>
2026-09-20 03:25:07 +0700

    feat(LastFmWidget): add line: <blank line>

--- components/LastFmWidget.tsx
+++ components/LastFmWidget.tsx
@@ -249,6 +249,7 @@ export default function LastFmWidget() {
 			}
 		});
 	}, [viewData, view]);
+
 		view === 'topartists' ? (viewData as ViewData['topartists'] | undefined) : undefined;
 
 	return (
