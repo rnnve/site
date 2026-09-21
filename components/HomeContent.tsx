d45a280ad3125d055429baab22b758a7bf0390e2
rnnve <fuyumi.am@gmail.com>
2026-09-21 12:09:26 +0700

    feat(HomeContent): add line 7: import SpotifyNowPlaying from './SpotifyNowPlaying';

--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -4,3 +4,4 @@ import { I18nProvider } from './I18nProvider';
 import { LiveProvider } from './LiveProvider';
 import DiscordProfileCard from './DiscordProfileCard';
 import AboutSection from './AboutSection';
+import SpotifyNowPlaying from './SpotifyNowPlaying';
