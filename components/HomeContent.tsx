3932fcecad81f96294770db9e44d6fd08de800ad
rnnve <fuyumi.am@gmail.com>
2026-09-21 12:09:27 +0700

    feat(HomeContent): add line 24: >

--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -21,3 +21,4 @@ function HomeInner() {
         <section
           className="flex min-h-0 min-w-0 flex-1 flex-col overflow-y-auto overscroll-contain"
           aria-label="Live status"
+        >
