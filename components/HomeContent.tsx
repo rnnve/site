213b5a18eaadabb09b9c5ad91956f65296063417
rnnve <fuyumi.am@gmail.com>
2026-09-21 12:09:26 +0700

    feat(HomeContent): add line 6: import AboutSection from './AboutSection';

--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -3,3 +3,4 @@
 import { I18nProvider } from './I18nProvider';
 import { LiveProvider } from './LiveProvider';
 import DiscordProfileCard from './DiscordProfileCard';
+import AboutSection from './AboutSection';
