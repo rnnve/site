96fd463b02da19eb09de597d0507308391be9ab0
rnnve <fuyumi.am@gmail.com>
2026-09-21 12:09:27 +0700

    feat(MusicIsland): add line 6: import type { LiveInitialData, LiveLastFmView } from '@/lib/integrati...

--- components/MusicIsland.tsx
+++ components/MusicIsland.tsx
@@ -3,3 +3,4 @@
 import { I18nProvider } from './I18nProvider';
 import { LiveProvider } from './LiveProvider';
 import MusicContent from './MusicContent';
+import type { LiveInitialData, LiveLastFmView } from '@/lib/integrations';
