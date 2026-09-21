b640a1424833a78dfa208130db720fb1e8774682
rnnve <fuyumi.am@gmail.com>
2026-09-21 12:09:27 +0700

    feat(HomeContent): add line 33: <SpotifyNowPlaying />

--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -30,3 +30,4 @@ function HomeInner() {
               <AboutSection />
             </div>
             <div className="min-w-0 rounded-2xl border border-outline-variant bg-surface-container/80 p-4 sm:p-5">
+              <SpotifyNowPlaying />
