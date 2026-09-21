1d2086a7836b13026a67a29d1ccf8dcf18cf4b82
rnnve <fuyumi.am@gmail.com>
2026-09-21 12:09:27 +0700

    feat(MusicIsland): add line 7: import { DocumentLanguageSync } from './DocumentLanguageSync';

--- components/MusicIsland.tsx
+++ components/MusicIsland.tsx
@@ -4,3 +4,4 @@ import { I18nProvider } from './I18nProvider';
 import { LiveProvider } from './LiveProvider';
 import MusicContent from './MusicContent';
 import type { LiveInitialData, LiveLastFmView } from '@/lib/integrations';
+import { DocumentLanguageSync } from './DocumentLanguageSync';
