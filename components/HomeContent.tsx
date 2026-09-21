35ad9386988b93077421533d0da7281e5da385ff
rnnve <fuyumi.am@gmail.com>
2026-09-21 12:09:27 +0700

    feat(HomeContent): add line 49: </LiveProvider>

--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -46,3 +46,4 @@ export default function HomeContent({ initialData, views }: HomeContentProps) {
       <LiveProvider initialData={initialData} views={views}>
         <DocumentLanguageSync />
         <HomeInner />
+      </LiveProvider>
