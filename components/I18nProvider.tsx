5a04b3cf0476e23cd4f8ac1b3ebe9873a5dcd2fe
rnnve <fuyumi.am@gmail.com>
2026-09-21 12:09:27 +0700

    fix(I18nProvider): add line: getLangSnapshot();

--- components/I18nProvider.tsx
+++ components/I18nProvider.tsx
@@ -132,6 +132,7 @@ export function I18nProvider({ children }: { children: ReactNode }) {
   const lang = useSyncExternalStore(subscribeLangChange, getLangSnapshot, getLangServerSnapshot);
 
   useEffect(() => {
+    getLangSnapshot();
   }, []);
 
   function setLang(next: Lang) {
