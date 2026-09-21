fb7d99391090cea3590a57ed3ef6aa8ef755ed98
rnnve <fuyumi.am@gmail.com>
2026-09-21 12:09:28 +0700

    fix(i18n): remove line: function setLang(next: Lang) {

--- lib/i18n.tsx
+++ lib/i18n.tsx
@@ -137,7 +137,6 @@ export function I18nProvider({ children }: { children: React.ReactNode }) {
 		getLangSnapshot();
 	}, []);
 
-	function setLang(next: Lang) {
 		setLangState(next);
 		if (typeof window !== 'undefined') {
 			window.localStorage.setItem(STORAGE_KEY, next);
