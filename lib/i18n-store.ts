6df59cd88c8f472d9836eecb19454d2b64269b81
rnnve <fuyumi.am@gmail.com>
2026-09-21 12:09:26 +0700

    feat(i18n-store): add line 76: }

--- lib/i18n-store.ts
+++ lib/i18n-store.ts
@@ -73,3 +73,4 @@ export function subscribeLangChange(callback: () => void): () => void {
 		window.removeEventListener(EVENT_NAME, onEvent);
 		window.removeEventListener('storage', onStorage);
 	};
+}
