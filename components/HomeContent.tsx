c63387755754c8389ce9d715b0c06a9dbcc59bc5
rnnve <fuyumi.am@gmail.com>
2026-09-21 12:09:26 +0700

    feat(HomeContent): add line 9: import type { LiveInitialData, LiveLastFmView } from '@/lib/integrati...

--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -6,3 +6,4 @@ import DiscordProfileCard from './DiscordProfileCard';
 import AboutSection from './AboutSection';
 import SpotifyNowPlaying from './SpotifyNowPlaying';
 import SiteFooter from './SiteFooter';
+import type { LiveInitialData, LiveLastFmView } from '@/lib/integrations';
